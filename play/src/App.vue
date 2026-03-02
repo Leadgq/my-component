35423097d1834ba7ffe064ddccbe40f69721aaff
gaoqiang <gaoqiang@hnprec.com>
2026-03-02 11:18:38 +0800

    refactor(play): 优化样式管理和按钮功能
    - 将全局样式从 App.vue 移至单独文件 style.css
    - 修改成功按钮点击事件为获取输入框焦点
    - 移除不再使用的追加按钮模板

--- play/src/App.vue
+++ play/src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1>组件库测试</h1>
     <my-button type="primary" @click="clickme">主要按钮</my-button>
-    <my-button type="success" @click="clickme">成功按钮</my-button>
+    <my-button type="success" @click="focus">获取焦点</my-button>
     <my-button type="danger" @click="clickme">危险按钮</my-button>
   </div>
   <div class="container">
@@ -13,9 +13,6 @@
           <el-option label="选项2" value="2" />
           <el-option label="选项3" value="3" />
         </el-select>
-      </template>
-      <template #append>
-        <el-button type="success" @click="clear">追加按钮</el-button>
       </template>
     </my-input>
   </div>
@@ -36,16 +33,11 @@ function handleChange(newValue: string) {
   console.log('inputValue 变化了:', newValue)
 }
 
-function clear() {
-  console.log(inputRef.value?.focus());
+function focus() {
+  inputRef.value?.focus()
 }
 
 function clickme() {
   console.log('ok')
 }
 </script>
-<style>
-:root{
-  --primary-color: blue;
-}
-</style>
