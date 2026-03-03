886f264d35577212c62223f247a7bef831acec71
gaoqiang <gaoqiang@hnprec.com>
2026-03-03 16:40:06 +0800

    feat(组件): 添加输入框组件并更新样式
    - 新增 MyInput 组件并实现基础功能
    - 在主题色中添加输入框中性色
    - 创建输入框样式文件并定义基础样式
    - 移除未使用的 primary-color 变量
    - 更新示例页面使用新输入框组件

--- play/src/App.vue
+++ play/src/App.vue
@@ -2,101 +2,15 @@
   <div class="container">
     <h1>组件库测试</h1>
     <div class="container">
-      <my-select v-model="value" multiple filterable remote reserve-keyword placeholder="Please enter a keyword"
-        remote-show-suffix :remote-method="remoteMethod" :loading="loading" style="width: 240px" :options="options">
-      </my-select>
+     <MyInput v-model="value" placeholder="请输入" style="width: 240px" clearable/>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 
-import { MySelect } from '../../packages/src/components/select'
+import { ref } from 'vue';
+import { MyInput } from '../../packages/src/components/input'
 
-import { onMounted, ref } from 'vue'
-
-interface ListItem {
-  value: string
-  label: string
-}
-
-const list = ref<ListItem[]>([])
-const options = ref<ListItem[]>([])
-const value = ref<string[]>([])
-const loading = ref(false)
-
-
-
-onMounted(() => {
-  list.value = states.map((item) => {
-    return { value: `value:${item}`, label: `label:${item}` }
-  })
-})
-
-const remoteMethod = (query: string) => {
-  if (query) {
-    loading.value = true
-    setTimeout(() => {
-      loading.value = false
-      options.value = list.value.filter((item) => {
-        return item.label.toLowerCase().includes(query.toLowerCase())
-      })
-    }, 200)
-  } else {
-    options.value = []
-  }
-}
-
-const states = [
-  'Alabama',
-  'Alaska',
-  'Arizona',
-  'Arkansas',
-  'California',
-  'Colorado',
-  'Connecticut',
-  'Delaware',
-  'Florida',
-  'Georgia',
-  'Hawaii',
-  'Idaho',
-  'Illinois',
-  'Indiana',
-  'Iowa',
-  'Kansas',
-  'Kentucky',
-  'Louisiana',
-  'Maine',
-  'Maryland',
-  'Massachusetts',
-  'Michigan',
-  'Minnesota',
-  'Mississippi',
-  'Missouri',
-  'Montana',
-  'Nebraska',
-  'Nevada',
-  'New Hampshire',
-  'New Jersey',
-  'New Mexico',
-  'New York',
-  'North Carolina',
-  'North Dakota',
-  'Ohio',
-  'Oklahoma',
-  'Oregon',
-  'Pennsylvania',
-  'Rhode Island',
-  'South Carolina',
-  'South Dakota',
-  'Tennessee',
-  'Texas',
-  'Utah',
-  'Vermont',
-  'Virginia',
-  'Washington',
-  'West Virginia',
-  'Wisconsin',
-  'Wyoming',
-]
+const value = ref('')
 </script>
