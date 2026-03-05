efffcbaaf3141ab8ed0a662641af02de7fc32156
Leadgq <2643336540@qq.com>
2026-03-05 18:33:55 +0800

    feat(组件注册): 添加自定义组件注册功能并支持重命名
    新增 registerCustomComponents 函数用于批量注册 Element Plus 组件并支持自定义前缀
    在组件类型声明中添加 Radio 相关组件
    移除 main.ts 中直接使用 ElementPlus 的代码

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,12 +1,23 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
+
 import App from './App.vue'
 
 import './style.css'
 
 const app = createApp(App)
 
+// import * as ElementPlus from 'element-plus'
 
-app.use(ElementPlus)
+// 获取所有组件 todo
+
+// const elComponents = {}
+// for (const key in ElementPlus) {
+//   if (key.startsWith('El')) {
+//     elComponents[key] = ElementPlus[key]
+//   }
+// }
+
+// console.log(elComponents);
+
 
 app.mount('#app')
